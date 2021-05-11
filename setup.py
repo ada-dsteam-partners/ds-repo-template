4a11d272b69469337be636a04f8b34c6ace4fd1a
Pavan Kumar <pavan.kumar@ada-asia.com>
2021-05-11 10:14:59 +0800

    Initial Template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Retail Business Insights Dashboard project repository for team colloboration',
+    author='DSTeam-ADA',
+    license='',
+)
